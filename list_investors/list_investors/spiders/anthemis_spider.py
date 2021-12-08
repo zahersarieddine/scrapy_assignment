2263f0a762546b3021036b03e7677a8783dbc14e
zahersarieddine <zaher.sarieddine@hotmail.com>
2021-12-08 10:52:48 +0200

    Update anthemis_spider.py

--- list_investors/list_investors/spiders/anthemis_spider.py
+++ list_investors/list_investors/spiders/anthemis_spider.py
@@ -1,8 +1,8 @@
 import scrapy
 import tldextract
 from urllib.parse import urljoin
-
 from list_investors.items import ListInvestorsItem
+
 class AnthemisInvestorsSpider(scrapy.Spider):
 
     name = "anthemis"
@@ -14,17 +14,16 @@ class AnthemisInvestorsSpider(scrapy.Spider):
         
     
     def parse(self, response):
-        # get all link elements that have are in div elements with class 'team-member' and then iterate over them to yield a separate 
-        # request that will be handled by another parser
+        # get all link elements that have are in div elements with class 'team-member' and iterate over them to yield a separate request.
+        
         investors = response.xpath("//*[contains(@class, 'team-member')]/a/@href").extract()
         for investor in investors:          
             url = urljoin(response.url, investor)          
             yield scrapy.Request(url, callback=self.parse_investor_page)      
                        
     def parse_investor_page(self, response):
+        # some investor pages include 'website' label and others include 'Website'. Translate the label to lower case before comparing it to 'website'
         item = ListInvestorsItem()
-        # some investor pages include 'website' label and others contain 'Website'. to allow both labels to pass the filter, translate the label to
-        # lower case before comparing it  
         inv_url = response.xpath("//a[contains(translate(.,'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'),'website')]/@href").get()
         item['name'] = tldextract.extract(inv_url).registered_domain
         yield item
